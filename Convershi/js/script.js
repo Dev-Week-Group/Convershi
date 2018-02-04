1e38eb7abf64662fa6249ddecadbe1ec6ef799f6
Ricky Reyes <richard.reyes@sjsu.edu>
2018-02-04 10:14:54 -0800

    added animations

--- Convershi/js/script.js
+++ Convershi/js/script.js
@@ -1,5 +1,35 @@
+$(document).ready(function(){
+    $(".slide-toggle").click(function(){
+        $(".wrapper").animate({
+        	width:"toggle"
+        });
+    });
+});
 
+	$('.slide-toggle').click(function(){
+		var $this = $(this);
+		$this.toggleClass('.slide-toggle');
+		if($this.hasClass('.slide-toggle')){
+			$this.text('>>');			
+		} else {
+			$this.text('<<');
+		}
+	});
 
+$(document).ready(function(){
+    $(".toggle-text").click(function(){
+        $(".recording").hide();
+        $(".form-group").show();
+        $(".submit-btn").show();
+    });
+});
+$(document).ready(function(){
+    $(".toggle-voice").click(function(){
+        $(".recording").show();
+        $(".form-group").hide();
+        $(".submit-btn").hide();
+    });
+});
 document.addEventListener( 'DOMContentLoaded', () => {
 
     // ===== cache DOM ===== //
